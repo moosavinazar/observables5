ed55f6009544ef1f1cd00c76e594cc787f58c0dd
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-16 00:04:18 +0330

    use filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./models/app-model";
+import {filter, from} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables5';
+export class AppComponent implements OnInit {
+
+  users: User[] = [
+    {
+      name: 'Foad',
+      age: 40,
+      status: 'active'
+    },
+    {
+      name: 'Rezvan',
+      age: 30,
+      status: 'active'
+    },
+    {
+      name: 'Ferion',
+      age: 28,
+      status: 'inactive'
+    },
+    {
+      name: 'Miad',
+      age: 32,
+      status: 'inactive'
+    },
+  ];
+
+  public ngOnInit() {
+
+    from(this.users).pipe(
+      filter(user => user.age < 35 && user.status === 'active')
+    ).subscribe(console.log);
+  }
+
 }
