79bc6212023fc112fd0f9f325b738af8d45f4410
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-16 00:34:35 +0330

    correct way to patch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {User} from "./models/app-model";
-import {delay, filter, from, fromEvent, map, of, tap} from "rxjs";
+import {concatMap, delay, filter, from, fromEvent, map, of, tap} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
@@ -43,19 +43,9 @@ export class AppComponent implements OnInit {
     fromEvent(this.input.nativeElement, 'input').pipe(
       map(event => event as InputEvent),
       map( item => (item.target as HTMLInputElement).value ),
+      concatMap(item => this.changeBodyPost(item)),
       tap( i => console.log(i) )
-    ).subscribe( val => {
-      let body = JSON.stringify({
-        body: val
-      });
-      let headers = new HttpHeaders({'Content-Type': 'application/json; charset=UTF-8'});
-      let options = { headers: headers }
-
-      this.http.patch(`https://jsonplaceholder.typicode.com/posts/${ val }`, body, options).pipe(
-        delay(2000)
-      ).subscribe(console.log);
-
-    });
+    ).subscribe();
 
     /*of(1, 2,3, 4, 5, 6, 7, 8, 9).subscribe(val => {
 
@@ -73,6 +63,18 @@ export class AppComponent implements OnInit {
     from(this.users).pipe(
       filter(user => user.age < 35 && user.status === 'active')
     ).subscribe(console.log);*/
+  }
+
+  public changeBodyPost(val: string) {
+    let body = JSON.stringify({
+      body: val
+    });
+    let headers = new HttpHeaders({'Content-Type': 'application/json; charset=UTF-8'});
+    let options = { headers: headers }
+
+    return this.http.patch(`https://jsonplaceholder.typicode.com/posts/1`, body, options).pipe(
+      delay(2000)
+    );
   }
 
 }
