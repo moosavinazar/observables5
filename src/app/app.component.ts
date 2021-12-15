d01a024729be3a117e5f1eedf94bc1927473aa8b
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-16 00:16:29 +0330

    use http.patch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from "./models/app-model";
-import {filter, from} from "rxjs";
+import {filter, from, of} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import {filter, from} from "rxjs";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient ) {
+  }
 
   users: User[] = [
     {
@@ -33,6 +37,19 @@ export class AppComponent implements OnInit {
   ];
 
   public ngOnInit() {
+
+    of(1, 2,3, 4, 5, 6, 7, 8, 9).subscribe(val => {
+
+      let body = JSON.stringify({
+        body: 'Test123',
+        title: 'Test Title'
+      });
+
+      let headers = new HttpHeaders({'Content-Type': 'application/json; charset=UTF-8'});
+      let options = { headers: headers }
+
+      this.http.patch(`https://jsonplaceholder.typicode.com/posts/${ val }`, body, options).subscribe(console.log)
+    });
 
     from(this.users).pipe(
       filter(user => user.age < 35 && user.status === 'active')
